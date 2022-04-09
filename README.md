2ed4bafede83b645aa761788d6f2d1b2f430ad99
Ricky12Awesome <rickygraveen@gmail.com>
2022-04-09 00:39:48 -0500

    README and some metadata

---
+++ README.md
@@ -0,0 +1,3 @@
+# Packwiz ModList
+
+Creates a modlist from [packwiz](https://packwiz.infra.link/)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use clap::Command;
+
 const CURSEFORGE_API: &str = "https://addons-ecs.forgesvc.net/api/v2/addon/search?gameId=432&pageSize=10&categoryId=0&sectionId=6";
 const MODRINTH_API: &str = "https://api.modrinth.com/v2/project";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-  let req = reqwest::get(CURSEFORGE_API).await?.text().await?;
-
-  println!("{req}");
+  let e = Command::new("");
 
   Ok(())
 }
