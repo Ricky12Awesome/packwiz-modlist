98b9787e6d593d14c4b6461230bf88582105b1fe
Ricky12Awesome <29931568+Ricky12Awesome@users.noreply.github.com>
2022-07-09 20:10:41 -0500

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-
+  // const URL: &str = "https://api.modrinth.com/v2/search?query=sodium";
+  //
+  // let res = minreq::get(URL)
+  //   .with_header("Content-Type", "application/json")
+  //   .send()
+  //   .unwrap();
+  //
+  // let res = res.as_str().unwrap();
+  //
+  //
+  //
+  // println!("{res}");
 }
